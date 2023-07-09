2e18c3988a90be39a16205a160c4618837555b9b
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-09 23:06:49 +0300

    Create Task_2_BeautifulString.java

---
+++ src/Task_2_BeautifulString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task_2_BeautifulString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        String str = in.next();
+
+        
+        //System.out.println(str);
+    }
+}
