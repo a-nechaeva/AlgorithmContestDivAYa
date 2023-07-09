70e6426a3c605e7ce323a4f46b4069eec57f4f44
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-10 00:28:32 +0300

    Update Task_2_BeautifulString.java

--- src/Task_2_BeautifulString.java
+++ src/Task_2_BeautifulString.java
@@ -6,7 +6,46 @@ public class Task_2_BeautifulString {
         int k = in.nextInt();
         String str = in.next();
 
+        int maxSequenceLen = 0;
+        char[] strArray = str.toCharArray();
         
-        //System.out.println(str);
+        for (int i = 0; i < strArray.length; i++) {
+            int currentSequenceLen = 0;
+            int start = i;
+            currentSequenceLen += 1;
+            int curK = k;
+
+            while (start < strArray.length - 2) {
+                while ((start < strArray.length - 2) && (strArray[start] == strArray[start + 1])) {
+                    currentSequenceLen += 1;
+                    start += 1;
+                }
+
+                while ((start < strArray.length - 2) && (strArray[start] != strArray[start + 1]) && (curK > 0)) {
+                    start += 1;
+                    currentSequenceLen +=1;
+                    curK -= 1;
+                }
+            }
+
+            if ((start == strArray.length - 1) && (curK > 0)) {
+                while (start > 1) {
+                    while ((start > 1) && (strArray[start] == strArray[start - 1])) {
+                        currentSequenceLen += 1;
+                        start -= 1;
+                    }
+
+                    while ((start > 1) && (strArray[start] != strArray[start - 1]) && (curK > 0)) {
+                        start -= 1;
+                        currentSequenceLen += 1;
+                        curK -= 1;
+                    }
+                }
+            }
+
+            maxSequenceLen = Math.max(maxSequenceLen, currentSequenceLen);
+
+        }
+        System.out.println(maxSequenceLen);
     }
 }
