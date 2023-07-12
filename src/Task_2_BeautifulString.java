61aad73f76065c325e26043472bec19f7a8f1a50
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-12 19:01:01 +0300

    task_3_passed))

--- src/Task_2_BeautifulString.java
+++ src/Task_2_BeautifulString.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 /*
@@ -14,7 +18,7 @@ public class Task_2_BeautifulString {
         Scanner in = new Scanner(System.in);
         int k = in.nextInt();
         String str = in.next();
-
+/*
         int maxSequenceLen = 0;
         char[] strArray = str.toCharArray();
 
@@ -81,5 +85,33 @@ public class Task_2_BeautifulString {
 
         }
         System.out.println(maxSequenceLen);
+
+ */
+//======================================================================================================================
+        //решение через префиксные суммы и двоичный поиск
+        //составим массив, входящих в строку символов -- алфавит
+        char[] st = str.toCharArray();
+        Character[] input = new Character[st.length];
+        for (int i = 0; i < st.length; i++) {
+            input[i] = st[i];
+        }
+        ArrayList<Character> alphabet = new ArrayList<>();
+
+        Arrays.stream(input).distinct().forEach(alphabet::add);
+
+        int maxSequenceLen = 0;
+
+        for (Character letter:  alphabet) {
+            // для каждого символа алфавита составляем массив префиксных сумм
+            int[] prefixSum = new int[st.length];
+            prefixSum[0] = (st[0] == letter) ? 0 : 1;
+
+            if (st.length > 1) {
+                for (int i = 1; i < st.length; i++) {
+                    prefixSum[i] = (st[i] == letter) ? prefixSum[i - 1] : prefixSum[i - 1] + 1;
+                }
+            }
+
+        }
     }
 }
--- src/Task_3_DiegoCollection.java
+++ src/Task_3_DiegoCollection.java
@@ -8,33 +8,62 @@ import java.util.*;
 собрал все наклейки с номерами не меньше, чем p_i. Вывести, сколько недастоющих каждому коллекционнеру наклеек есть
 у Диего, не считая повторные экземпляры.
 
-Ниже неэффективный по времени алгоритм :(
+Ниже эффективный по времени алгоритм :)
  */
 public class Task_3_DiegoCollection {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int diegoHas = Integer.parseInt(reader.readLine());
-        ArrayList<Integer> diegoStickers = new ArrayList<>();
-        String[] inputD = reader.readLine().split("\\s");
-        Arrays.stream(inputD).mapToInt(Integer::parseInt).distinct().sorted().forEach(diegoStickers::add);
+        if (diegoHas == 0) {
+            String empty = reader.readLine();
+            int numberOfCollecForEmpty = Integer.parseInt(reader.readLine());
+            String inpt = reader.readLine();
+            if (numberOfCollecForEmpty != 0) {
+                for (int i = 0; i < numberOfCollecForEmpty; i++)
+                    System.out.println(0);
+            }
+        } else {
+            ArrayList<Integer> diegoStickers = new ArrayList<>();
+            String[] inputD = reader.readLine().split("\\s");
+            Arrays.stream(inputD).mapToInt(Integer::parseInt).distinct().sorted().forEach(diegoStickers::add);
 
-        int numberOfCollec = Integer.parseInt(reader.readLine());
-        //HashMap<Integer, Integer> stickersCount = new HashMap<>();
-        String[] inputK = reader.readLine().split("\\s");
-        ArrayList<Integer> minCollectHave = new ArrayList<>();
-        Arrays.stream(inputK).mapToInt(Integer::parseInt).forEach(minCollectHave::add);
+            int numberOfCollec = Integer.parseInt(reader.readLine());
+            if (numberOfCollec == 0) {
+                String nobodyVisitDiego = reader.readLine();
+            } else {
+                String[] inputK = reader.readLine().split("\\s");
+                ArrayList<Integer> minCollectHave = new ArrayList<>();
+                Arrays.stream(inputK).mapToInt(Integer::parseInt).forEach(minCollectHave::add);
 
-        for (Integer integer : minCollectHave) {
-            int countSticker = 0;
-            for (int j = diegoStickers.size() - 1; j >= 0; j--) {
-                if (diegoStickers.get(j) < integer) {
+                for (Integer minP : minCollectHave) {
+                    int countSticker = 0;
+                    // здесь двоичный поиск вместо перебора
+                    int left = -1;
+                    int right = diegoStickers.size();
+                    while ((left < right - 1)) {
+
+                        int curIndex = (left + right) / 2;
+                        if (diegoStickers.get(curIndex) < minP) {
+                            left = curIndex;
+                            countSticker = curIndex + 1;
+                        } else {
+                            right = curIndex;
+                        }
+                    }
+
+
+           /* for (int j = diegoStickers.size() - 1; j >= 0; j--) {
+                if (diegoStickers.get(j) < minP) {
                     countSticker = j + 1;
                     break;
                 }
             }
-            System.out.println(countSticker);
+            */
+                    System.out.println(countSticker);
 
+                }
+            }
         }
     }
 }
