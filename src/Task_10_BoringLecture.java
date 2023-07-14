fd964c90a7e8588483667c23bb941583168968e1
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-14 18:51:04 +0300

    Create Task_10_BoringLecture.java

---
+++ src/Task_10_BoringLecture.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task_10_BoringLecture {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String alexFavouriteWord = in.next();
+    }
+
+}
