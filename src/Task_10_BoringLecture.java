53d6eb082a6b1856ed18f63517b7d68e2f147276
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-16 23:13:15 +0300

    Update Task_10_BoringLecture.java

--- src/Task_10_BoringLecture.java
+++ src/Task_10_BoringLecture.java
@@ -1,10 +1,27 @@
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Task_10_BoringLecture {
+    // не проходит 9 тест (WA)
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Hashtable<Character, Integer> alphabet = new Hashtable<>();
+        char[] input = in.next().toCharArray();
+        Character[] alexFavouriteWord = new Character[input.length];
 
-        String alexFavouriteWord = in.next();
+        // почему не работает автоупаковка char в Character? или как обойтись без дополнительных массивов?
+        for (int i = 0; i < input.length; i++) {
+            alexFavouriteWord[i] = input[i];
+        }
+
+        Arrays.stream(alexFavouriteWord).distinct().forEach(s -> alphabet.put(s, 0));
+
+        for (int i = 0; i < input.length; i++) {
+            int wordInStringsCounter = (i + 1) * (input.length - i);
+            alphabet.put(alexFavouriteWord[i], alphabet.get(alexFavouriteWord[i]) + wordInStringsCounter);
+        }
+        Arrays.stream(alphabet.keySet().toArray()).sorted().forEach(s -> System.out.println(s + ": " + alphabet.get(s)));
     }
 
 }
